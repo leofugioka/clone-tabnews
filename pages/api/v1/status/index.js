33a1f106879f35ae64c25f1dabe65732bc736341
Leandro Fugioka <77177218+leofugioka@users.noreply.github.com>
2025-11-19 14:13:36 -0300

    feat(database.js): Add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   res
     .status(200)
     .json({ mensagem: "alunos do curso.dev são pessoas acima da média" });
