6c7f475d0b461c2319214d061fca41fbc93d0c20
Leandro Fugioka <77177218+leofugioka@users.noreply.github.com>
2025-11-24 09:43:14 -0300

    feat(database.js): add a more robust ssl config

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-  console.log(`Credenciais do Postgres:`, {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValue(),
   });
 
   try {
@@ -28,6 +20,15 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValue() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
 }
 
 export default {
